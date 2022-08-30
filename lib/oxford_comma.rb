5768df5115692e9a03b61c8279f48e9b190ad2aa
Ezra-code <njerukithinji872@gmail.com>
2022-08-30 22:10:59 +0300

    commit tree b2aeecb0fdbac13f329d3e35d6765d23b7798d5c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,15 @@
+# def oxford_comma(array)
+#   return array.join(' and ') if array.size < 3
+#   array[-1] = "and " + array[-1]
+#   array.join(', ')
+# end
+
+
 def oxford_comma(array)
-  return array.join(' and ') if array.size < 3
-  array[-1] = "and " + array[-1]
-  array.join(', ')
+    if array.length < 3
+       return array.join(" and ")
+    else
+        array[-1] = "and " + array[-1]
+       return array.join(", ")
+    end
 end
